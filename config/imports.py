c3b272409f3da96ba4627371906609104febbbc9
rubensrvsc <rubens.rvsc@gmail.com>
2022-01-26 20:50:03 -0300

    env: add new key in config app

--- config/imports.py
+++ config/imports.py
@@ -1,12 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_jwt_extended import (
   JWTManager,
 )
 from flask_sqlalchemy import SQLAlchemy
-from settings import JWT_SECRET_KEY, DB_PATH
+from .settings import JWT_SECRET_KEY, DB_PATH
 
 app = Flask(__name__)
-app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY  # Change this!
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_PATH
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_PATH
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 jwt = JWTManager(app)
 db = SQLAlchemy(app)

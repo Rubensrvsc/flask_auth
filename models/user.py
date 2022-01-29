27f310c9c181dd294f57991b134532d071c1c590
rubensrvsc <rubens.rvsc@gmail.com>
2022-01-29 20:55:43 -0300

    refactor: models relationship

--- models/user.py
+++ models/user.py
@@ -6,10 +6,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    tasks = db.relationship('Task', backref='task', lazy=True, primaryjoin="foreign(User.id) == remote(Task.task_id)")
+    tasks = db.relationship('Task',
+        backref=db.backref('user', lazy=True))
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f"User: {self.username}"
     
     @property
     def serialize(self):
@@ -21,5 +22,8 @@ class User(db.Model):
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name_task = db.Column(db.String(200), nullable=False)
-    task_id = db.Column(db.Integer, db.ForeignKey('task.id'),
-        nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Task: {self.name_task}"

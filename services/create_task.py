83f740363ec056de9006e60174f72a3e8b64eb66
rubensrvsc <rubens.rvsc@gmail.com>
2022-02-03 21:37:12 -0300

    feat: steps of create_task

--- services/create_task.py
+++ services/create_task.py
@@ -3,3 +3,15 @@ from returns.pipeline import flow, is_successful
 from returns.pointfree import bind
 from models.user import User
 
+def get_user():
+  pass
+
+def validate_params():
+  pass
+
+def insert_task():
+  pass
+
+def create_task():
+  pass
+
